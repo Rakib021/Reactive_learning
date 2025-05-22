bf15f6c0e29399f6ecdefa00674f8c2b05c57b0a
Rakib <rakib@Rakibs-MacBook-Pro.local>
2025-05-22 17:21:37 +0600

    rendering list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import Gallary from "./components/Gallary"
-import TodoList from "./components/TodoList"
 
-
+const people = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+}, {
+  id: 3,
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',  
+}, {
+  id: 4,
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+}];
 function App() {
+const chemists = people.filter(person =>person.profession==="chemist");
 
+const listItems = chemists.map(chemist=><li key={chemist.id}>{chemist.name}</li>)
 
   return (
-   <div>
-
-    <TodoList />
-   </div>
+<div>
+{listItems}
+</div>
   )
 }
 
---
+++ src/components/Avatar.jsx
@@ -0,0 +1,10 @@
+export default function Avatar(size){
+  return(
+    <div>
+      <h1 width={size} height={size}>
+This file contain mainly avatar
+      </h1>
+     
+    </div>
+  );
+}
